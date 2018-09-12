25a1ddc787b53168b1cbaf107a361277d1a066ec
Scott M <gmat191@gmail.com>
2018-09-12 00:10:10 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -2,5 +2,5 @@
 # returns the phrase
 def parrot(phrase="Squawk!")
   puts phrase
-  return phrase #our return disrupts the flow of our program and instead of outputting => nil in irb, it replaces it with 
+  return phrase #our return disrupts the flow of our program and instead of outputting => nil in irb, it replaces it with "Squawk!"
 end
